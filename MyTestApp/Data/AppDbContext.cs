73dae394c02b21420e0b41335e41763a2dacb169
Evgeniy Shtykhlin <evgeniyshtykhlin@gmail.com>
2025-02-16 17:05:28 +0500

    Initial commit

---
+++ MyTestApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace MyTestApp.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MyTestApp.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
